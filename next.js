4a8e78608ba9e9f062e988bc5a8f0839f076f8f6
Bertram Truong <b@bertramtruong.com>
2016-01-11 15:37:45 +1100

    initial commit

---
+++ next.js
@@ -0,0 +1,2 @@
+var c = document.getElementsByTagName('frame')[1].contentDocument.getElementsByTagName('frame')[1].contentDocument.getElementsByTagName('frame')[0].contentDocument.getElementsByTagName('frame')[1].contentDocument.getElementsByTagName('a');
+c[c.length - 1].click();
